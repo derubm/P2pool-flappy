7c21a0a9f3f90421deebe171658dcea1853ee4e3
James Donald <jdonald@leapmotion.com>
2017-07-22 16:15:00 -0700

    Fix block explorer URLs, except the unsupported address=

--- p2pool/bitcoin/networks/flappycoin.py
+++ p2pool/bitcoin/networks/flappycoin.py
@@ -20,9 +20,9 @@ POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoW
 BLOCK_PERIOD = 60 # s
 SYMBOL = 'FLAP'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Flappycoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Flappycoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.flappycoin'), 'flappycoin.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'http://blockcrawler.rf.gd/index.php'
-ADDRESS_EXPLORER_URL_PREFIX = 'http://blockcrawler.rf.gd/index.php'
-TX_EXPLORER_URL_PREFIX = 'http://blockcrawler.rf.gd/index.php'
+BLOCK_EXPLORER_URL_PREFIX = 'http://blockcrawler.rf.gd/index.php?block_hash='
+ADDRESS_EXPLORER_URL_PREFIX = 'http://blockcrawler.rf.gd/index.php?address='
+TX_EXPLORER_URL_PREFIX = 'http://blockcrawler.rf.gd/index.php?transaction='
 SANE_TARGET_RANGE = (2**256//1000000000 - 1, 2**256//1000 - 1)
 DUMB_SCRYPT_DIFF = 2**16
 DUST_THRESHOLD = 0.03e8
