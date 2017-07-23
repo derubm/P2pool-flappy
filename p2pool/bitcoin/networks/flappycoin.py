6f9c15a56bc0106e9f3916f83c935233356e5464
James Donald <jdonald@leapmotion.com>
2017-07-23 16:13:51 -0700

    Remove extraneous comma, and correct subsidy func

--- p2pool/bitcoin/networks/flappycoin.py
+++ p2pool/bitcoin/networks/flappycoin.py
@@ -15,7 +15,7 @@ RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
             'flappycoinaddress' in (yield bitcoind.rpc_help()) and
             not (yield bitcoind.rpc_getinfo())['testnet']
         ))
-SUBSIDY_FUNC = lambda height: 50*100000000 (height + 1)//750000,
+SUBSIDY_FUNC = lambda height: 50
 POW_FUNC = lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data))
 BLOCK_PERIOD = 60 # s
 SYMBOL = 'FLAP'
